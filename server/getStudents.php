a9d1a8c97d9081a1e429c4be9df137bb021ea133
Michael Salvati <print@3daddict.com>
2018-09-23 17:17:47 -0700

    added read of all records

--- server/getStudents.php
+++ server/getStudents.php
@@ -1,5 +1,34 @@
 <?php
 
-// readfile('../assets/read.json');
+require_once('mysql_credentials.php');
+
+$query = 'SELECT * FROM students';
+
+$result = mysqli_query($conn, $query);
+$output = [
+    'success' => false,
+    'students' => [],
+    'errors' => []
+];
+
+if($result) {
+    //query connection is successfull
+    if(mysqli_num_rows($result) > 0){
+        //query returned data
+        $output['success'][] = true;
+        //ask Dan about a safer way to do this loop to protect from sql injection attacks
+        while($row = mysqli_fetch_assoc($result)){
+            $output['students'][] = $row;
+        }
+    } else {
+        //the query data is empty!?!
+        $output['errors'][] = 'no data availible';
+    }
+} else {
+    $output['errors'][] = "Error with query";
+}
+
+$json_output = json_encode($output);
+print($json_output);
 
 ?>
